1d6963b40ae35a136674739d343e6a4679bf8ad0
Patrick Kenekayoro <ktarila@gmail.com>
2021-05-27 22:35:32 +0100

    some updates

--- assets/controllers/moreads_controller.js
+++ assets/controllers/moreads_controller.js
@@ -17,10 +17,15 @@ export default class extends Controller {
             // Get new ads
             let ad_container = doc.getElementById('advert-container');
             // append new add to document
-            console.log(ad_container);
+            // console.log(ad_container);
             let children = (ad_container.children);
             for (let child of children) {
-                console.log(child);
+                // update has tags
+                var containsTag = child.querySelectorAll('.has-tag');
+                for (var i = containsTag.length - 1; i >= 0; i--) {
+                    let text = containsTag[i].innerHTML;
+                    containsTag[i].innerHTML = text.replace(/#(\w+)/g, '<a class="hover:text-deep-orange-600 text-blue-500 dark:text-blue-400" href="/tag/$1">#$1</a>');;
+                }
                 document.getElementById('advert-container').appendChild(child);
             }
             // replace loadmore button
